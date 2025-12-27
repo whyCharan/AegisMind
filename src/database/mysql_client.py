0448c5c777dc30c879f93328367d94d1f98a9c9d
Vinayaka Charan <charan.sandaka5@gmail.com>
2025-12-27 20:36:55 +0530

    Database Connection is ready

---
+++ src/database/mysql_client.py
@@ -0,0 +1,15 @@
+import os 
+import mysql.connector
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_connection():
+
+    connection = mysql.connector.connect(
+        host = os.getenv('MYSQL_HOST'),
+        user = os.getenv('MYSQL_USER'),
+        password = os.getenv('MYSQL_PASSWORD'),
+        database = os.getenv('MYSQL_DATABASE')
+    )
+    return connection
